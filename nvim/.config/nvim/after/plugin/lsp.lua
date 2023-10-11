d9e196384e7e7ccf0856780a54551d36794ede71
Stoyan Shukerov <stoichik@gmail.com>
2023-10-11 13:25:23 -0700

    Add more keybinding to LSP config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,9 +1,15 @@
 local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
 
 lsp_zero.on_attach(function(client, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp_zero.default_keymaps({buffer = bufnr})
+
+  
+  vim.keymap.set('n', '<leader>gr', '<cmd>Telescope lsp_references<cr>', {buffer = bufnr, remap = false})
+  vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, {buffer = bufnr, remap = false})
 end)
 
 require('mason').setup({})
@@ -13,3 +19,15 @@ require('mason-lspconfig').setup({
 		lsp_zero.default_setup,
 	},
 })
+
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- 'Enter' key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+  })
+})
+
