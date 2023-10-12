b91d60eb9225a85af5a7351cda1c541b481a3203
Stoyan Shukerov <stoichik@gmail.com>
2023-10-11 19:12:48 -0700

    Add helpful keybinds
    Keybinds for:
      - open explorer
      - move lines around when highlighted
      - modify default J behavior to keep cursor inplace
      - C-d and C-u keep cursor in middle of the terminal
      - when searching keeps found term in the middle of the terminal
      - preserve paste when pasting over
      - copy to system clipboard

--- nvim/.config/nvim/lua/blabla/remap.lua
+++ nvim/.config/nvim/lua/blabla/remap.lua
@@ -1,2 +1,29 @@
 vim.g.mapleader = ","
+
+
+-- open explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move lines around when highlighted
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- modify default J behavior to keep cursor inplace
+vim.keymap.set("n", "J", "mzJ`z")
+
+
+-- C-d and C-u keep cursor in middle of the terminal
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+
+-- when searching keeps found term in the middle of the terminal
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- preserve paste when pasting over
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- copy to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
